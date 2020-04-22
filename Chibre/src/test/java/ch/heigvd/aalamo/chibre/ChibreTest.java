aec089beb7a7ef6166a084cdce5cd3835c2a2845
alex-mottier <alexandre.mottier@heig-vd.ch>
2020-04-22 17:45:14 +0200

    Comments

--- Chibre/src/test/java/ch/heigvd/aalamo/chibre/ChibreTest.java
+++ Chibre/src/test/java/ch/heigvd/aalamo/chibre/ChibreTest.java
@@ -1,3 +1,11 @@
+/* ---------------------------
+Projet de Génie Logiciel (GEN) - HEIG-VD
+Fichier :     ChibreTest.java
+Auteur(s) :   Alexis Allemann, Alexandre Mottier
+Date :        01.04.2020 - 11.06.2020
+But : Classe permettant de faire les tests unitaires
+Compilateur : javac 11.0.4
+--------------------------- */
 package ch.heigvd.aalamo.chibre;
 
 import ch.heigvd.aalamo.chibre.engine.*;
