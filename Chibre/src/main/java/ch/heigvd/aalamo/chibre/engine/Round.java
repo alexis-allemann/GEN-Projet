9814391fa354c17ce720a0279a600264b4f4cd6e
alex-mottier <alexandre.mottier@heig-vd.ch>
2020-05-02 14:53:28 +0200

    T34 T35 Update de round

--- Chibre/src/main/java/ch/heigvd/aalamo/chibre/engine/Round.java
+++ Chibre/src/main/java/ch/heigvd/aalamo/chibre/engine/Round.java
@@ -15,26 +15,56 @@ import java.util.List;
 
 public class Round {
     // Attributs
+    private final int id;
+    private static int count = 1;
     private CardColor trumpColor;
     private List<Turn> turns = new ArrayList<>(Game.NB_CARDS_PLAYER);
-    private CardCollection cardCollection = new CardCollection();
+    private final CardCollection cardCollection = new CardCollection();
+    private final Game game;
+    private boolean isPlayed = true; // TODO : remettre à zéro quand le round est fini
+    Player firstPlayerTrump;
 
     /**
      * Instancier un tour de jeu
      */
     public Round(Game game) {
-        //TODO :
-        // 1. Récupèrer les joueurs
-        // 2. Distribuer les cartes à chaque joueur
-        // 3. Choisir l'atout (le joueur à la suite et le premier tour celui qui a le 7 de carreau)
-        // test
+        this.game = game;
+        this.id = count++;
+    }
 
-        Player trumpPlayer = game.getTrumpPlayer();
+    public Player getTrumpPlayer() {
+        return game.getTable().nextTrumpPlayer(id, game.getTable().getPlayerPosition(firstPlayerTrump));
+    }
 
-        for (Player player : game.getPlayers()) {
-            boolean hasDiamondSeven = cardCollection.distributeCards(player, Game.NB_CARDS_PLAYER);
-            if(trumpPlayer == null && hasDiamondSeven)
-                trumpPlayer = player;
+    public void run(){
+        
+        for(Player player : game.getPlayers()){
+            if(cardCollection.distributeCards(player, Game.NB_CARDS_PLAYER) && game.getRounds().size() == 1)
+                firstPlayerTrump = player;
         }
+
+        while(!playerCardsEmpty()){
+            Turn turn = new Turn(this);
+            turns.add(turn);
+            turn.run();
+        }
+
+        isPlayed = false;
+    }
+
+    private boolean playerCardsEmpty() {
+        for(Player player : game.getPlayers()){
+            if(player.getCards().size() > 1)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean isPlayed() {
+        return isPlayed;
+    }
+
+    public Game getGame() {
+        return game;
     }
 }
